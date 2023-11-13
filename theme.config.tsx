5486ae2ecf681137aa808a95e8e4ad31cb62e881
Jeffrey Morris <ffejcsirrom@gmail.com>
2023-11-13 01:29:22 -0600

    update nextra theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,54 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import styled from 'styled-components';
+
+const GradientTextWrapper = styled.span`
+  background: linear-gradient(315deg, #8e44ad, #3498db, #1abc9c);
+  -webkit-background-clip: text;
+  color: transparent;
+  display: inline;
+  font-weight: bold;
+`;
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <GradientTextWrapper>AuroraBotics</GradientTextWrapper>,
+  docsRepositoryBase: 'https://github.com/aurorabotics/docs/tree/main',
+  primaryHue: 282,
+  primarySaturation: 61,
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s - AuroraBotics Docs'
+      }
+    }
+  },
+  banner: {
+    dismissible: false,
+    text: '⚠️ This website is currently under construction. ⚠️'
+  },
   project: {
     link: 'https://github.com/aurorabotics/docs',
   },
   chat: {
-    link: 'https://discord.com/mtx6cuhVMt',
+    link: 'https://discord.gg/mtx6cuhVMt',
   },
-  docsRepositoryBase: 'https://github.com/aurorabotics/docs',
+  sidebar: {
+    toggleButton: true
+  },
+  navigation: false,
   footer: {
-    text: 'AuroraBotics Documentation',
+    text: <span>
+      GNU General Public License v3.0 {new Date().getFullYear()} ©{' '}
+      <a href="https://aurorabotics.com" target="_blank">
+        <GradientTextWrapper>AuroraBotics</GradientTextWrapper>
+      </a>
+      .
+    </span>
   },
 }
 
 export default config
+
+
