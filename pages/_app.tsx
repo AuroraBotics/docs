0a59ce06f4a604f6cc2ab87e02eb1813b30bf081
Jeffrey Morris <ffejcsirrom@gmail.com>
2024-05-17 21:36:42 -0500

    change to next link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,17 @@ import { useState, useEffect } from 'react'
 import { NextUIProvider } from '@nextui-org/react'
 import { Image, Link } from '@nextui-org/react'
 import NextImage from 'next/image'
-import { StrictMode } from 'react'
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function MyApp({ Component, pageProps }) {
+
+    const router = useRouter();
+
     return (
-        <StrictMode>
-            <NextUIProvider>
-                <Component components={customMDX} {...pageProps} />
-            </NextUIProvider>
-        </StrictMode>
+        <NextUIProvider>
+            <Component components={customMDX} {...pageProps} />
+        </NextUIProvider>
     )
 }
 
@@ -38,14 +40,15 @@ const customMDX = {
         return img;
     },
     a: (href: { href: string, children: string }) => {
-        var external = !(href.href.startsWith("./") || href.href.startsWith("#") || href.href.includes("aurorabotics.com") || href.href.startsWith("localhost"));
+        var external = !(href.href.startsWith("/") || href.href.startsWith("./") || href.href.startsWith("#") || href.href.includes("aurorabotics.com") || href.href.startsWith("localhost"));
         return (
             <Link
                 // isBlock
                 underline='hover'
                 showAnchorIcon={external}
                 isExternal={external}
-                href={href.href}
+                as={NextLink}
+                href={href.href ?? ''}
             >{href.children}</Link>
         )
     }
